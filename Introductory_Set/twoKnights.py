9aecff8159a4dd00127cd33cf9fa69f1f04a5f4d
Ashwin <bhanageashwin28@gmail.com>
2025-09-11 11:03:59 +0530

    fix(twoKnights): Fix the errors in the loop for the output

--- Introductory_Set/twoKnights.py
+++ Introductory_Set/twoKnights.py
@@ -3,6 +3,8 @@ input = sys.stdin.readline
 
 n = int(input())
 for i in range(1, n+1):
-    total_pairs = k*k + (k*k - 1) // 2
-    attack_pairs = 4 * (k-1) * (k-2)
+    total_pairs = i*i * (i*i - 1) // 2
+    attack_pairs = 4 * (i-1) * (i-2)
     print(total_pairs - attack_pairs)
+
+
